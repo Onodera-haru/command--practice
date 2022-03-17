a168cf8cee08b065fa72ec9282fd2839e1ce1e74
Haru Onodera <h.onodera.sys21@morijyobi.ac.jp>
2022-03-17 12:32:41 +0900

    Main.javaの変更

--- Main.java
+++ Main.java
@@ -4,11 +4,13 @@ public class Main {
 	public static void main(String [] args) {
 		Scanner scanner = new Scanner(System.in);
 		double tall,weight,bmi;
+		for(int i = 0;i <= 1;i++){
 		tall =  scanner.nextDouble();
 		weight = scanner.nextInt();
 		bmi = (weight/tall)/tall;
 		bmi = ((double)Math.round(bmi * 10))/10;
 		System.out.println(bmi);
+		}
 		
 	}
 }
